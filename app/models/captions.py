002d2a37a0a291a4627f2972ccab6bc6a9706cc8
Asifuzzaman Asif <asif.softvence@gmail.com>
2025-07-19 00:01:49 +0600

    New variable added like platform, post type and post topic

--- app/models/captions.py
+++ app/models/captions.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 class CaptionInput(BaseModel):
-    business_type: str
-    post_description: str
-    season: str
+
+    business_type: Optional[str] = None 
+    post_description: Optional[str] = None 
+    season: Optional[str] = None 
     location: Optional[str] = None
+    platforms: List[str]  # facebook, instagram, linkedin
+    post_topic: Optional[str] = None  # e.g. "customer spotlight"
+    post_type: Optional[str] = None  
